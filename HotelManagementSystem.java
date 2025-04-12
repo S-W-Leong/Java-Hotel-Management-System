16b7f0ffa28c48b1ea5c2a044f86f5e89f70a24a
S-W-Leong <shiweileong0.0@gmail.com>
2025-04-12 14:46:02 +0800

    Added java files

---
+++ HotelManagementSystem.java
@@ -0,0 +1,20 @@
+package V3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+// Main class to run the hotel management system
+public class HotelManagementSystem {
+
+    public static void main(String[] args) {
+        HotelSystem system = new HotelSystem();
+        system.start();
+    }
+}
+
+
+
+
+
+
---
+++ Staff.java
@@ -0,0 +1,11 @@
+package V3;
+
+// Staff class
+abstract class Staff extends User {
+    protected String role;
+
+    public Staff(String username, String password, String role) {
+        super(username, password);
+        this.role = role;
+    }
+}
